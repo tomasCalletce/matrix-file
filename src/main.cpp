8af64409c3f0a286a3a40c25997754c7b045a1ee
juan-c-s <juancamilo.salazar2002@gmail.com>
2024-04-07 19:32:02 -0500

    JuanControllerRead

--- src/main.cpp
+++ src/main.cpp
@@ -8,19 +8,23 @@
 #include <cmath>
 #include <chrono>
 #include <thread>
+// #include "./utils/Pixel.cpp"
 
 
-#include "./FileReader.cpp"
-#include "./FileWriter.cpp"
+#include "./BMPController.cpp"
+#include "./JuanController.cpp"
 
-
+using namespace std;
 int main(int argc, char* argv[]) {
+    // PARA CORRER
+    //./A [NOMBRE_ARCHIVO_LECTURA] [NOMBRE_ARCHIVO_ESCRITURA] [ID] [DATE]
+    //./A ../img/raw/boat.bmp ../img/encoded/compressedImg.juan 10101 2024-04-07T23:09:15Z
 
-    FileReader prueba = FileReader();
+    BMPController prueba = BMPController();
     vector<vector<Pixel>> pict = prueba.leerArchivoBMP(argv[1]);
     int rows = pict.size(), cols = pict[0].size();
 
-    FileWriter writer = FileWriter();
+    JuanController writer = JuanController();
     writer.write(pict, argv[2], argv[3], argv[4]);
     
     return 0;
---
+++ src/utils/Pixel.cpp
@@ -0,0 +1,5 @@
+struct Pixel {
+    unsigned char blue;
+    unsigned char green;
+    unsigned char red;
+};
