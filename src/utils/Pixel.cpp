7e37cb5d2fa18b255bea5cc725164aa2ed6c70ab
tomas <64665732+tomasCalletce@users.noreply.github.com>
2024-05-29 17:13:06 -0500

    feat: pixel to string

--- src/utils/Pixel.cpp
+++ src/utils/Pixel.cpp
@@ -1,5 +1,5 @@
-
 #include <vector>
+#include <string>
 
 using namespace std;
 
@@ -8,18 +8,23 @@ struct Pixel {
     unsigned char green;
     unsigned char red;
 
-    Pixel(){}
-    Pixel(unsigned char blue,
-    unsigned char green,
-    unsigned char red):blue(blue), green(green), red(red){}
-    Pixel operator^(vector<int> a){
+    Pixel() {}
+    Pixel(unsigned char blue, unsigned char green, unsigned char red)
+        : blue(blue), green(green), red(red) {}
+
+    Pixel operator^(vector<int> a) {
         return Pixel{((int)blue ^ a[0]), ((int)green ^ a[1]), ((int)red ^ a[2])};
     }
-    
-    // Pixel operator^(vector<int> a){
-    //     return {((int)blue - a[0] + 256) % 256, ((int)green - a[1] + 256)%256, ((int)red - a[2] + 256)%256};
-    // }
-    string getString(){
-        
+
+    static std::string pictToString(const std::vector<std::vector<Pixel>>& pict) {
+        std::string result;
+        for (const auto& row : pict) {
+            for (const auto& pixel : row) {
+                result += static_cast<char>(pixel.blue);
+                result += static_cast<char>(pixel.green);
+                result += static_cast<char>(pixel.red);
+            }
+        }
+        return result;
     }
 };
